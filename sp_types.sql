0b74e6650a5e3ffdd12f7608b91cce3545af8f6a
Bernardo Nacif <89922059+bernardonacif@users.noreply.github.com>
2023-08-14 20:58:44 -0300

    Create sp_types.sql

---
+++ sp_types.sql
@@ -0,0 +1,47 @@
+-- Here are some examples of common "sp_" commands in SQL Server and their functions: | Aqui estão alguns exemplos de comandos "sp_" comuns no SQL Server e suas funções:
+
+
+-- English
+
+sp_help -- Returns information about a specific database object, such as tables, columns, indexes, stored procedures, etc.
+
+sp_who -- Returns information about current user sessions connected to the database, including details like session ID, session status, user name, and more.
+
+sp_rename -- Used to rename database objects, such as tables or columns.
+
+sp_adduser / sp_addlogin -- Used to create new users or logins on the server.
+
+sp_depends -- Displays a list of objects that depend on a given object (for example, tables and columns that depend on a specific view).
+
+sp_spaceused -- Returns information about the space used by an object in the database.
+
+sp_executesql -- Executes a dynamically constructed SQL statement.
+
+sp_helptext -- Returns the complete text of an object's definition, such as the source code of a stored procedure.
+
+sp_configure -- Used to configure server options.
+
+sp_recompile -- Forces the recompilation of a stored procedure or related object.
+
+  
+-- Portugues/BR
+
+sp_help -- Retorna informações sobre um objeto específico do banco de dados, como tabelas, colunas, índices, procedimentos armazenados etc.
+
+sp_who -- Retorna informações sobre as sessões de usuário atuais conectadas ao banco de dados, incluindo detalhes como ID da sessão, status da sessão, nome do usuário, entre outros.
+
+sp_rename -- Usado para renomear objetos de banco de dados, como tabelas ou colunas.
+
+sp_adduser / sp_addlogin -- Usados para criar novos usuários ou logins no servidor.
+
+sp_depends -- Exibe uma lista de objetos que dependem de um determinado objeto (por exemplo, tabelas e colunas que dependem de uma exibição específica).
+
+sp_spaceused -- Retorna informações sobre o espaço usado por um objeto no banco de dados.
+
+sp_executesql -- Executa uma instrução SQL dinamicamente construída.
+
+sp_helptext -- Retorna o texto completo de uma definição de objeto, como o código-fonte de um procedimento armazenado.
+
+sp_configure -- Usado para configurar opções de servidor.
+
+sp_recompile -- Força a recompilação de um procedimento armazenado ou objeto relacionado.

3c677b6113771516553a30f72b7111a3418a7246
Bernardo Nacif <89922059+bernardonacif@users.noreply.github.com>
2024-01-16 11:53:21 -0300

    Update consumo_proc_v2.sql
    add session_id

--- consumo_proc_v2.sql
+++ consumo_proc_v2.sql
@@ -9,3 +9,19 @@ CROSS APPLY
     sys.dm_exec_sql_text(s.sql_handle) AS qt
 ORDER BY 
     s.total_worker_time DESC;
+
+
+------ add session_id
+
+SELECT 
+    s.execution_count,
+    s.total_worker_time AS TotalWorkerTime,
+    s.total_worker_time / s.execution_count AS AvgWorkerTime,
+    OBJECT_NAME(qt.objectid, dbid) AS ProcedureName,
+    s.session_id
+FROM 
+    sys.dm_exec_procedure_stats s
+CROSS APPLY 
+    sys.dm_exec_sql_text(s.sql_handle) AS qt
+ORDER BY 
+    s.total_worker_time DESC;
